527fe414ec129f8e14944bca9be9e8ed034fc778
karan14ary <karan.14ary@gmail.com>
2022-11-30 13:17:43 +0530

    first commit

---
+++ src/tnsbasic/Demo.java
@@ -0,0 +1,10 @@
+package tnsbasic;
+
+public class Demo {
+    public static void main(String[] args) {
+        int a=10,b=60;
+        System.out.println("Core Java 8");
+        System.out.println("Streampi,Collections");
+        System.out.println("Addition is "+(a+b));
+    }
+}
